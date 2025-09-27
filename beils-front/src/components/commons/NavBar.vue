dc430a48f9a107ecfd7f1b3e20ada2373492da73
Robert Esgalla <robert.hernandez@esgalla.com>
2025-09-27 17:12:24 +0200

    Initial template

---
+++ beils-front/src/components/commons/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="navbar bg-base-100 shadow-sm">
+		<a class="btn btn-ghost text-xl">daisyUI</a>
+	</div>
+</template>
+
+<style scoped></style>
---
+++ beils-front/src/pages/Dashboard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+	import NavBar from '@/components/commons/NavBar.vue'
+	import MainLayout from '@/components/layouts/MainLayout.vue'
+</script>
+
+<template>
+	<MainLayout>
+		<template #header>
+			<NavBar />
+		</template>
+		<template #content>
+			<div>Dashboard</div>
+		</template>
+		<template #footer>
+			<div>Dashboard</div>
+		</template>
+	</MainLayout>
+</template>
